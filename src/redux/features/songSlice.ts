cdaae225376a17323d1ab9823c8460530d2be44a
kirubel <kirubeljalleta62@gmail.com>
2023-10-10 16:01:00 +0300

    debug

--- src/redux/features/songSlice.ts
+++ src/redux/features/songSlice.ts
@@ -81,7 +81,9 @@ export const getSongListAction = createAsyncThunk("song/getAll", async () => {
   //   Authorization: "Bearer my-token",
   //   "My-Custom-Header": "foobar",
   // };
-  const response = await axios.get(`${process.env.REACT_APP_RESTAPI_GET_ALL}`);
+  const response = await axios.get(
+    "https://songbackend-2l9j.onrender.com/songs/all"
+  );
   const responseArray = response.data.data;
   console.log("woev");
   console.log(responseArray);
